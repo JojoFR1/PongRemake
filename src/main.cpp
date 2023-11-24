a5003be77864552048449f398d1ca2b986a73bd3
JojoFR1 <JojoFR1@users.noreply.github.com>
2023-11-24 19:30:09 +0100

    Score & Text + middle Line

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,28 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Pong Remake", sf::Style::Titlebar | sf::Style::Close);
     sf::Event event;
+    sf::Font font;
+    if (!font.loadFromFile("src/arial.ttf"));
+    sf::Vertex line[2];
+
+    line[0].position = { window.getSize().x / 2.f, 0.f};
+    line[1].position = { window.getSize().x / 2.f, (float) window.getSize().y};
 
     float ballSpeedX = .35f;
     float ballSpeedY = .35f;
     float paddleSpeed = 1.f;
 
+    int scoreRight = 0;
+    sf::Text textRight(std::to_string(scoreRight), font);
+    textRight.setPosition({ window.getSize().x - window.getSize().x / 4.f - 45.f, 50.f});
+    textRight.setCharacterSize(90);
     sf::RectangleShape paddleRight({ 10.f, 100.f });
     paddleRight.setPosition({ 50.f - paddleRight.getSize().x / 2.f, window.getSize().y / 2.f - paddleRight.getSize().y / 2.f});
 
+    int scoreLeft = 0;
+    sf::Text textLeft (std::to_string(scoreLeft), font);
+    textLeft.setPosition({ window.getSize().x / 4.f - 45.f, 50.f});
+    textLeft.setCharacterSize(90);
     sf::RectangleShape paddleLeft({ 10.f, 100.f });
     paddleLeft.setPosition({ window.getSize().x - 50.f + paddleLeft.getSize().x / 2.f, window.getSize().y / 2.f - paddleLeft.getSize().y / 2.f});
 
@@ -59,14 +73,28 @@ int main() {
             ballSpeedY *= -1.f;
         if (ball.getPosition().y + ball.getRadius() >= window.getSize().y)
             ballSpeedY *= -1.f;
+
+        if (ball.getPosition().x < 0.f) {
+            scoreRight += 1;
+            textRight.setString(std::to_string(scoreRight));
+            ball.setPosition({ window.getSize().x / 2.f, window.getSize().y / 2.f});
+        }
+        if (ball.getPosition().x > window.getSize().x) {
+            scoreLeft += 1;
+            textLeft.setString(std::to_string(scoreLeft));
+            ball.setPosition({ window.getSize().x / 2.f, window.getSize().y / 2.f});
+        }
         
         ball.move(sf::Vector2f(1.f * ballSpeedX, 1.f * ballSpeedY));
     
         window.clear();
 
+        window.draw(line, 2, sf::Lines);
         window.draw(ball);
         window.draw(paddleRight);
         window.draw(paddleLeft);
+        window.draw(textRight);
+        window.draw(textLeft);
 
         window.display();
     }
